4b61a029fdbd443b06f5cfc58faf164ab97a3262
Edward Smith <esmith78@gmail.com>
2023-05-27 15:19:46 -0400

    arrow functions

---
+++ index.js
@@ -0,0 +1,20 @@
+// has a function expression called divide.
+
+
+const divide = function() {
+    let a = 2000;
+    let b = 100;
+    return 2000 / 100;
+
+}
+
+const square = () => {
+    let x = 2;
+    return x * x;
+}
+
+
+
+const add = (a, b) => {
+    return a + b;
+}
